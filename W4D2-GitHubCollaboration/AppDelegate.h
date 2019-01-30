880ff5b5e890523e435329b9983d46d5070fc1e9
Pavel Uvarov <p.uvarov@gmail.com>
2019-01-30 11:16:24 -0500

    Create new project

---
+++ W4D2-GitHubCollaboration/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  W4D2-GitHubCollaboration
+//
+//  Created by Pavel on 2019-01-30.
+//  Copyright © 2019 Pavel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ W4D2-GitHubCollaboration/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  W4D2-GitHubCollaboration
+//
+//  Created by Pavel on 2019-01-30.
+//  Copyright © 2019 Pavel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
